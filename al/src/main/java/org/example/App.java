a664138e81416166091ab9915402920961c244ee
napasun <wkzkfmxk23@gmail.com>
2020-08-15 03:06:16 +0900

    개구리가 뛴 횟수, 가장 빠르게 갈 수 있는 수

--- al/src/main/java/org/example/App.java
+++ al/src/main/java/org/example/App.java
@@ -8,12 +8,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        int [] a = {1,2,3,4,5};
-        int [] result = solution(null, 0);
-
-        for (int item: result) {
-            System.out.println(item);
-        }
+//        int [] a = {1,2,3,4,5};
+//        int [] result = solution(null, 0);
+//
+//        for (int item: result) {
+//            System.out.println(item);
+//        }
+        int [] b = {1, 3, 1, 4, 2, 3, 5, 4};
+        System.out.println(solution(20, 50, 20));
+        System.out.println(solution(5, b));
     }
 
     static public int[] solution(int[] A, int K) {
@@ -37,5 +40,44 @@ public class App
         }
 
         return result;
+    }
+
+//    public int solution(int X, int Y, int D) {
+//        // write your code in Java SE 8
+//        int result = 0;
+//
+//        while(X < Y) {
+//            X += D;
+//            ++result;
+//        }
+//
+//        return result;
+//    }
+
+    static public int solution(int X, int Y, int D) {
+        if (Y - X == 0) return 0;
+
+        int midValue = (Y - X) / D;
+        if ((Y - X) % D != 0) midValue += 1;
+
+        return midValue;
+    }
+
+    static public int solution(int X, int[] A) {
+        // write your code in Java SE 8
+        int r = -1;
+        int[] result = new int[X];
+
+        for (int i = 0; i < A.length; i++) {
+            if (result[A[i] - 1] == 0) result[A[i] -1] = i+1;
+        }
+
+        for (int temp: result) {
+            if (temp == 0) return -1;
+
+            if (r < temp) r = temp;
+        }
+
+        return r-1;
     }
 }
